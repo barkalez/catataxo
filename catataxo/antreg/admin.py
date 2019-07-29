8f6d7e23ff160e993a474a10355c7c9ecee36dbe
barkalez <barkala21@hotmail.com>
2019-07-29 16:58:06 +0200

    Preparado para Hektor

--- catataxo/antreg/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Especie, Genero
-
-# Register your models here.
-
-admin.site.register(Especie)
-admin.site.register(Genero)
--- catataxo/antreg/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AntregConfig(AppConfig):
-    name = 'antreg'
